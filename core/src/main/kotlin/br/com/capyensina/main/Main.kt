daf2c4cc61458725ff6784dcc58eec6faf0238c0
MalcolnLMR <malcolnlucas@gmail.com>
2024-11-06 21:21:41 -0300

    Add screens to Main.kt

--- core/src/main/kotlin/br/com/capyensina/main/Main.kt
+++ core/src/main/kotlin/br/com/capyensina/main/Main.kt
@@ -1,6 +1,11 @@
 package br.com.capyensina.main
 
+import br.com.capyensina.main.ui.screens.BooksScreen
+import br.com.capyensina.main.ui.screens.EditScreen
 import br.com.capyensina.main.ui.screens.HomeScreen
+import br.com.capyensina.main.ui.screens.InvestimentoScreen
+import br.com.capyensina.main.ui.screens.LojaScreen
+import br.com.capyensina.main.ui.screens.RpgScreen
 import ktx.app.KtxGame
 import ktx.app.KtxScreen
 import ktx.async.KtxAsync
@@ -11,8 +16,12 @@ class Main : KtxGame<KtxScreen>() {
         KtxAsync.initiate()
 
         addScreen(HomeScreen(this))
+        addScreen(RpgScreen(this))
+        addScreen(BooksScreen(this))
+        addScreen(EditScreen(this))
+        addScreen(LojaScreen(this))
+        addScreen(InvestimentoScreen(this))
+
         setScreen<HomeScreen>()
-
-
     }
 }
