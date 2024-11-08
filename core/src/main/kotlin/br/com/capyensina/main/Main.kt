b1dac50a91927c49a174dada891eba5e904b6ac7
MalcolnLMR <malcolnlucas@gmail.com>
2024-11-08 15:36:25 -0300

    fix typo in main.kt

--- core/src/main/kotlin/br/com/capyensina/main/Main.kt
+++ core/src/main/kotlin/br/com/capyensina/main/Main.kt
@@ -25,7 +25,7 @@ class Main : KtxGame<KtxScreen>() {
     override fun create() {
         KtxAsync.initiate()
 
-        // Initialize UI utils, must at least earlier than Layouts
+        // Initialize UI utils, at least, must be earlier than Layouts
         skinManager = SkinManager()
 
         // Initialize Layouts
@@ -43,7 +43,7 @@ class Main : KtxGame<KtxScreen>() {
         addScreen(QuizScreen(this))
         addScreen(SplashScreen(this))
 
-        // Set start screen, must at least be after adding screens
+        // Set start screen, at least, must be after adding screens
         //setScreen<SplashScreen>()
         setScreen<HomeScreen>()
     }
