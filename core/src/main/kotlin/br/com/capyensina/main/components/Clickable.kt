2848e44e7e96b455f5f12cd980832c2bdaf8f1ea
MalcolnLMR <malcolnlucas@gmail.com>
2024-11-12 20:11:17 -0300

    Update Clickable.kt
    remove unused imports

--- core/src/main/kotlin/br/com/capyensina/main/components/Clickable.kt
+++ core/src/main/kotlin/br/com/capyensina/main/components/Clickable.kt
@@ -2,7 +2,6 @@ package br.com.capyensina.main.components
 
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.math.Rectangle
-import ktx.assets.toInternalFile
 
 data class Clickable(
     var texture: Texture,
